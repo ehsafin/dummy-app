1101da0f850a288a3aa956fca9ddc5f8e9716bcf
ehsafin <safin@studiodevhub.com>
2024-01-15 15:18:21 +0600

    feat: added eas update support

--- App.js
+++ App.js
@@ -1,7 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import * as Updates from "expo-updates";
+import { useEffect } from "react";
+
+async function onFetchUpdateAsync() {
+  try {
+    const update = await Updates.checkForUpdateAsync();
+
+    if (update.isAvailable) {
+      await Updates.fetchUpdateAsync();
+      await Updates.reloadAsync();
+    }
+  } catch (error) {
+    alert(`Error fetching latest update: ${error}`);
+  }
+}
 
 export default function App() {
+  useEffect(() => {
+    onFetchUpdateAsync();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -13,8 +32,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
